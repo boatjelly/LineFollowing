75823c29ee2916dc5d1d15a8ffc508380e669644
horstmanj34 <horstmanj34@dupage.edu>
2025-04-21 10:40:00 -0500

    Add files via upload

--- StepByStep.ino
+++ StepByStep.ino
@@ -2,6 +2,7 @@
   ~~~1 Step at a time~~~
   By: Sasha Dauz, Jacob Horstman
   Written: April 19, 2025
+  Edited: April 20, 2025
   I/O
   A4:  [INPUT] Right Wheel Encoder
   A5:  [INPUT] Left Wheel Encoder
@@ -11,9 +12,9 @@
   D6:  [OCR0A] Left Motor Reverse Speed; Connects to H-Bridge Pin 2
   D11: [OCR2A] Right Motor Reverse Speed; Connects to Pin 10 on the H-Bridge
 */
-volatile unsigned long counterRight = 0;
-volatile unsigned long counterLeft = 0;
-volatile unsigned long avg = 0;
+volatile unsigned char counterRight = 0;
+volatile unsigned char counterLeft = 0;
+volatile unsigned char avg = 0;
 volatile unsigned char rightA = 1;
 volatile unsigned char rightB = 1;
 volatile unsigned char leftA = 1;
@@ -29,40 +30,41 @@ void setup() {
   PCMSK1 = 0x30;  // A4, A5
   // Enable internal pull-up
   PORTC |= 0x30;  // A4, A5
-  
+
   // Left Motor Setup
   TCCR0A = 0xA1;    // Configure Timer 0 for Phase-Correct PWM (Left motor)
-                    // 8-bit
-                    // TOP value 0xFF
+  // 8-bit
+  // TOP value 0xFF
   TCCR0B = 0x01;
   // Right Motor Setup
   TCCR2A = 0xA1;    // Configure Timer 2 for PWM (Right motor)
-                    // 8-bit
-                    // TOP value 0xFF
+  // 8-bit
+  // TOP value 0xFF
   TCCR2B = 0x01;
   // Configure output pins
   DDRD = 0x68;  // D3, D5, D6 as output for motors
   DDRB = 0x08;  // D11 as output for motor control
-  
+
   // Re-enable global interrupts
   sei();
 }
 
-void moveForward(){
+void moveForward() {
+  counterLeft() = 0;
+  counterRight() = 0;
+
   // Configure OCRnB
   OCR0B = 237;       // Left motor forward
   OCR2B = 255;       // Right motor forward
-  
-  // while(ticks); // Like to move forward by 1m
-  
+  while (avg != 192)
+    ;
   // Clear OCRnB
   OCR0B = 0;         // Clear left motor
   OCR2B = 0;         // Clear right motor
 }
 
 void loop() {
-  // moveForward();
-  _delay_ms(3000);
+  moveForward();
 }
 
 ISR(PCINT1_vect) {
@@ -76,5 +78,17 @@ ISR(PCINT1_vect) {
   }
   leftA = leftB;
   rightA = rightB;
-  avg = (counterLeft + counterRight)/2;
+  avg = (counterLeft + counterRight) >> 1; // avg range [0,192]
 }
+
+/*
+ * 
+ *   // Reset counters after one revolution
+  // Hopefully only an unsigned char will be needed to store
+  if (counterRight == 193) {
+    counterRight = 0;
+  }
+  if (counterLeft == 193) {
+    counterLeft = 0;
+  }
+ */
